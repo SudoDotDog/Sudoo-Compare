1fcba285c6cbeb3208eed78772394e73fcb716d9
WMXPY <wm@mengw.io>
2020-09-17 11:52:42 -0500

    style

--- src/compare.ts
+++ src/compare.ts
@@ -10,10 +10,10 @@ import { createCompareResult } from "./util";
 export const compare = (left: any, right: any): CompareResult[] => {
 
     if (typeof left !== 'object' || typeof right !== 'object') {
+
         if (left === right) {
             return [];
         }
-
         return [createCompareResult([], left, right)];
     }
 
