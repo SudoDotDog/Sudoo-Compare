f11ca18d2254cf0b0fdf996dc02891628f677ee0
WMXPY <wm@mengw.io>
2020-09-17 18:56:00 -0500

    add object unit tests

--- test/unit/compare.test.ts
+++ test/unit/compare.test.ts
@@ -66,4 +66,45 @@ describe('Given [Compare] Helper methods', (): void => {
             createCompareResult([], left, right),
         ]);
     });
+
+    it('should be able to compare object - happy path', (): void => {
+
+        const key: string = chance.string();
+        const value: string = chance.string();
+
+        const left: Record<string, string> = {
+            [key]: value,
+        };
+        const right: Record<string, string> = {
+            [key]: value,
+        };
+        const result: CompareResult[] = compare(left, right);
+
+        expect(result).to.be.lengthOf(0);
+    });
+
+    it('should be able to compare object - sad path', (): void => {
+
+        const key: string = chance.string();
+        const leftValue: string = chance.string();
+        const rightValue: string = chance.string();
+
+        const commonKey: string = chance.string();
+        const commonValue: string = chance.string();
+
+        const left: Record<string, string> = {
+            [commonKey]: commonValue,
+            [key]: leftValue,
+        };
+        const right: Record<string, string> = {
+            [commonKey]: commonValue,
+            [key]: rightValue,
+        };
+        const result: CompareResult[] = compare(left, right);
+
+        expect(result).to.be.lengthOf(1);
+        expect(result).to.be.deep.equal([
+            createCompareResult([key], leftValue, rightValue),
+        ]);
+    });
 });
